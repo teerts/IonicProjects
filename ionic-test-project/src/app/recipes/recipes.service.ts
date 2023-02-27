1850fda37e4343c734c76def855cc01f37433ad7
Gerry Grewall <gobind99@gmail.com>
2023-02-26 19:59:14 -0800

    Move recipes data to recipes service and create helper method to getAllRecipes()
    - return a copy of the recipes found in the the recipes obj

--- ionic-test-project/src/app/recipes/recipes.service.ts
+++ ionic-test-project/src/app/recipes/recipes.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class RecipesService {
 
   constructor() { }
+
+  private recipes: Recipe[] = [
+    {
+    id: 'r1',
+    title: 'Tonkotsu Ramen',
+    imageUrl: "https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg",
+    ingredients: ['Sesame Oil', 'Avocado Oil', 'Garlic', 'Fresh Ginger', 'Pork', 'Rice Vinegar', 'Soft-boiled Egg']
+    },
+    {
+      id: 'r1',
+      title: 'Chicken Ramen',
+      imageUrl: "https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg",
+      ingredients: ['Sesame Oil', 'Avocado Oil', 'Garlic', 'Fresh Ginger', 'Chicken', 'Rice Vinegar', 'Soft-boiled Egg']
+      }
+  ];
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
 }
