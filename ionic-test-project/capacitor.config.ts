ae3a3e607c5f057d156ea2ce896c263868580669
Gerry Grewall <gobind99@gmail.com>
2023-03-12 20:03:56 -0700

    Add Capacitor to run Android emulator
    - Added capacitor to run android emulator. Live reload does not work but base deployment is fine.

---
+++ ionic-test-project/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic-project.recipelist-project',
+  appName: 'ionic-test-project',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
